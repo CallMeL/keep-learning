cd91eac21751de67c7f17c2009838d4ffd9e93f4
Chen <chenxi2.71@qq.com>
2021-02-01 17:28:49 +0800

    cpp learning

---
+++ learing C demo.cpp
@@ -0,0 +1,8 @@
+//
+//  learing C demo.cpp
+//  
+//
+//  Created by 陈汐 on 2021/2/1.
+//
+
+#include "learing C demo.hpp"
---
+++ learing C demo.hpp
@@ -0,0 +1,13 @@
+//
+//  learing C demo.hpp
+//  
+//
+//  Created by 陈汐 on 2021/2/1.
+//
+
+#ifndef learing_C_demo_hpp
+#define learing_C_demo_hpp
+
+#include <stdio.h>
+
+#endif /* learing_C_demo_hpp */
